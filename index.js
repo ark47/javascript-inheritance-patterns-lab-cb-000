9b888f00084df3d1ee4058e5d9d7966e32ba83ea
Chris Nunez <cenunez47@gmail.com>
2018-04-10 23:20:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,71 @@
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Point.prototype.toString = function() {
+  return `(${this.x}, ${this.y})`;
+};
+
+function Side(length) {
+  this.length = length;
+}
+
+function Shape() {
+
+}
+
+Shape.prototype.addToPlane = function(x, y) {
+  this.position = new Point(x, y);
+};
+
+Shape.prototype.move = function(x, y) {
+  this.position = new Point(x, y);
+};
+
+function Circle(radius) {
+  Shape.call(this);
+  this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.diameter = function() {
+  return (this.radius * 2);
+};
+
+Circle.prototype.area = function() {
+  return (Math.PI * this.radius ^ 2);
+};
+
+Circle.prototype.circumference = function() {
+  return (2 * Math.PI * this.radius); 
+};
+
+function Polygon(sides) {
+  Shape.call(this);
+  this.sides = sides;
+}
+
+Polygon.prototype = Object.create(Shape.prototype);
+Polygon.prototype.constructor = Polygon;
+
+Polygon.prototype.perimeter = function() {
+  let num = 0;
+  for (let i = 0; i < this.sides.length; i++) {
+    num += this.sides[i].length;
+  }
+  return num;
+};
+
+Polygon.prototype.numberOfSides = function() {
+  return this.sides.length;
+};
+
+function Triangle(side1, side2, side3) {
+  Polygon.call(this, [new Side(side1), new Side(side2), new Side(side3)]);
+}
+
+Triangle.prototype = Object.create(Polygon.prototype);
+Triangle.prototype.constructor = Triangle;
